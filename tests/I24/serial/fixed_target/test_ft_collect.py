91a6026a46c1a81344bd82c579474fb47e859d0d
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2024-05-15 13:09:10 +0100

    Fix test

--- tests/I24/serial/fixed_target/test_ft_collect.py
+++ tests/I24/serial/fixed_target/test_ft_collect.py
@@ -77,14 +77,18 @@ def test_get_prog_number(chip_type, map_type, pump_repeat, expected_prog):
         ),  # Map irrelevant, pp to Repeat2, checker enabled
     ],
 )
-@patch("mx_bluesky.I24.serial.fixed_target.i24ssx_Chip_Collect_py3v1.i24.pmac")
 @patch("mx_bluesky.I24.serial.fixed_target.i24ssx_Chip_Collect_py3v1.caget")
 def test_load_motion_program_data(
-    fake_caget, fake_pmac, map_type, pump_repeat, checker, expected_calls
+    fake_caget,
+    map_type,
+    pump_repeat,
+    checker,
+    expected_calls,
 ):
     test_dict = {"N_EXPOSURES": [0, 1]}
     fake_caget.return_value = checker
-    fake_pmac.pmac_string = MagicMock()
+    fake_pmac = MagicMock()
+    # fake_pmac.pmac_string = MagicMock()
     load_motion_program_data(fake_pmac, test_dict, map_type, pump_repeat)
     call_list = []
     for i in expected_calls:
