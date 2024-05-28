917cd3c84dad8edd66056b9e7a4a673c34967a9d
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2024-05-28 13:37:45 +0100

    Wait for blueapi before starting edm - to be tested

--- src/mx_bluesky/I24/serial/run_fixed_target.sh
+++ src/mx_bluesky/I24/serial/run_fixed_target.sh
@@ -6,12 +6,32 @@ edm_path=$1
 # Get the directory of this script
 current=$( realpath "$( dirname "$0" )" )
 
-# Start rabbitMQ here 
+# Start rabbitMQ here - Using i23-control instead
 # podman run -it --rm --net host rmohr/activemq:5.15.9-alpine
 
 # Start the blueapi worker using the config file in this module
 echo "Starting the blueapi runner"
 blueapi -c "${current}/blueapi_config.yaml" serve &
+
+# Wait for blueapi to start
+for i in {1..30}
+do
+    echo "$(date)"
+    curl --head -X GET http://localhost:25565/status >/dev/null
+    ret_value=$?
+    if [ $ret_value -ne 0 ]; then
+        sleep 1
+    else
+        break
+    fi
+done
+
+if [ $ret_value -ne 0 ]; then
+    echo "$(date) BLUEAPI Failed to start!!!!"
+    exit 1
+else
+    echo "$(date) BLUEAPI started"
+fi
 
 # Open the edm screen for a fixed target serial collection
 echo "Starting fixed target edm screen."
