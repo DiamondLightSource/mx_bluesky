f5c6df1f04d77b1afc6e8e66083de78e26945d57
Noemi Frisina <noemi.frisina@diamond.ac.uk>
2024-06-04 16:47:35 +0100

    Have the common script for starting blueapi called by run_fixed_target and extruder

--- src/mx_bluesky/I24/serial/run_extruder.sh
+++ src/mx_bluesky/I24/serial/run_extruder.sh
@@ -6,29 +6,8 @@ edm_path=$1
 # Get the directory of this script
 current=$( realpath "$( dirname "$0" )" )
 
-# Start the blueapi worker using the config file in this module
-echo "Starting the blueapi runner"
-blueapi -c "${current}/blueapi_config.yaml" serve &
-
-# Wait for blueapi to start
-for i in {1..30}
-do
-    echo "$(date)"
-    curl --head -X GET http://localhost:25565/status >/dev/null
-    ret_value=$?
-    if [ $ret_value -ne 0 ]; then
-        sleep 1
-    else
-        break
-    fi
-done
-
-if [ $ret_value -ne 0 ]; then
-    echo "$(date) BLUEAPI Failed to start!!!!"
-    exit 1
-else
-    echo "$(date) BLUEAPI started"
-fi
+# Run script to start blueapi serve
+. $current/start_blueapi.sh
 
 # Open the edm screen for an extruder serial collection
 echo "Starting extruder edm screen."
--- src/mx_bluesky/I24/serial/run_fixed_target.sh
+++ src/mx_bluesky/I24/serial/run_fixed_target.sh
@@ -9,29 +9,8 @@ export EDMDATAFILES="/dls_sw/prod/R3.14.12.3/support/motor/6-7-1dls14/motorApp/o
 # Get the directory of this script
 current=$( realpath "$( dirname "$0" )" )
 
-# Start the blueapi worker using the config file in this module
-echo "Starting the blueapi runner"
-blueapi -c "${current}/blueapi_config.yaml" serve &
-
-# Wait for blueapi to start
-for i in {1..30}
-do
-    echo "$(date)"
-    curl --head -X GET http://localhost:25565/status >/dev/null
-    ret_value=$?
-    if [ $ret_value -ne 0 ]; then
-        sleep 1
-    else
-        break
-    fi
-done
-
-if [ $ret_value -ne 0 ]; then
-    echo "$(date) BLUEAPI Failed to start!!!!"
-    exit 1
-else
-    echo "$(date) BLUEAPI started"
-fi
+# Run script to start blueapi serve
+. $current/start_blueapi.sh
 
 # Open the edm screen for a fixed target serial collection
 echo "Starting fixed target edm screen."
