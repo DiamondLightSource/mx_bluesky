6b5f1a76b921264de387ddb03f3ed0eac5773829
Oliver Silvester <oliver.silvester@diamond.ac.uk>
2024-09-16 18:23:20 +0100

    increase panda pulse width

--- tests/unit_tests/hyperion/device_setup_plans/test_setup_panda.py
+++ tests/unit_tests/hyperion/device_setup_plans/test_setup_panda.py
@@ -13,6 +13,7 @@ from ophyd_async.fastcs.panda import SeqTrigger
 
 from mx_bluesky.hyperion.device_setup_plans.setup_panda import (
     MM_TO_ENCODER_COUNTS,
+    PULSE_WIDTH_US,
     disarm_panda_for_gridscan,
     set_panda_directory,
     setup_panda_for_flyscan,
@@ -139,8 +140,6 @@ def test_setup_panda_correctly_configures_table(
     ][0]
 
     table = table_msg.args[0]
-
-    PULSE_WIDTH_US = 1
     SPACE_WIDTH_US = int(time_between_x_steps_ms * 1000 - PULSE_WIDTH_US)
     expected_seq_rows: list[SeqRow] = [
         SeqRow(1, SeqTrigger.BITA_1, 0, 0, 0, 1, 0),
