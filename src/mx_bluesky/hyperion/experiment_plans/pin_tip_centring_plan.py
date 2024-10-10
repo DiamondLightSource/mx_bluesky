e2779ce7f4743b817fea7aeb65a1a7188ca87543
David Perl <david.perl@diamond.ac.uk>
2024-10-10 13:46:17 +0100

    ignore many ophyd classic typing mismatches

--- src/mx_bluesky/hyperion/experiment_plans/pin_tip_centring_plan.py
+++ src/mx_bluesky/hyperion/experiment_plans/pin_tip_centring_plan.py
@@ -154,7 +154,7 @@ def pin_tip_centre_plan(
     tip = yield from move_pin_into_view(pin_tip_detect, smargon)
     yield from offset_and_move(tip)
 
-    yield from bps.mvr(smargon.omega, 90)
+    yield from bps.mvr(smargon.omega, 90)  # type: ignore # See: https://github.com/bluesky/bluesky/issues/1809
 
     # need to wait for the OAV image to update
     # See #673 for improvements
